3168775725a99c0b04d2f6957a5307195679c577
Wishforpeace <90396662+Wishforpeace@users.noreply.github.com>
2023-02-11 23:00:54 +0800

    Revert "s"

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # 2023-Shmily-backend
-
 Backend Repository for Shmily
-王严妍
--- model/migrate.go
+++
@@ -1,8 +0,0 @@
-package model
-
-func migration() {
-	//自动迁移、创建数据库表
-	DB.Set("gorm:table_options", "charset=utf8mb4").AutoMigrate(&User{}).AutoMigrate(&Memo{})
-	DB.Model(&Memo{}).AddForeignKey("Uid", "User(id)", "CASCADE", "CASCADE")
-
-}
